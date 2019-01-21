bd7608b7ee925d54d832442a0ed7cbaf610a2a79
fahad-gcet <fms525@outlook.com>
2019-01-21 14:47:28 +0530

    Initial Commit

---
+++ src/components/common/Header.js
@@ -0,0 +1,15 @@
+import React, {PropTypes} from 'react';
+import {IndexLink} from 'react-router';
+
+
+
+const Header = () => {
+  return (
+    <nav>
+      <IndexLink to="/" activeClassName="active">Home</IndexLink>
+    </nav>
+  );
+};
+
+
+export default Header;
---
+++ src/components/home/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>This is homepage</h1>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
